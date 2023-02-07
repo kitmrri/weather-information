0bb71a8c534a7e467f968f6e911ffd52df084555
kitmrri <manuntagkit@gmail.com>
2023-02-07 16:17:22 +0900

    add current forecast

--- spa/src/components/SelectedCity.vue
+++ spa/src/components/SelectedCity.vue
@@ -2,6 +2,7 @@
   
   <div>
     <h2>Selected City: {{ selectedCity }}</h2>
+    <h3>Current Weather: {{ currentWeather.main.temp }} - {{ currentWeather.weather[0].description }} </h3>
     <h3>5-Day Forecast:</h3>
     <ul>
       <li v-for="forecast in fiveDayForecast">
@@ -18,7 +19,8 @@ export default {
   data() {
     return {
       axios: inject("axios"),
-      fiveDayForecast: []
+      fiveDayForecast: [],
+      currentWeather: []
     };
   },
   watch: {
@@ -32,6 +34,15 @@ export default {
   methods: {
     fetchWeatherData() {
       if (this.selectedCity) {  
+        this.axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.selectedCity}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`)
+          .then(response => {
+            this.currentWeather = response.data;
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.error(error);
+          });
+
         this.axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${this.selectedCity}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`)
           .then(response => {
             this.fiveDayForecast = response.data.list;
