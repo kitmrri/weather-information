fd61499f28312cf62cb237c8c90d1244d3e37f5c
kitmrri <manuntagkit@gmail.com>
2023-02-08 20:57:33 +0900

    fix the border height on selectedcity

--- spa/src/components/WeatherInformation.vue
+++ spa/src/components/WeatherInformation.vue
@@ -16,10 +16,11 @@
       </div>
     </aside>
     <div class="w-full p-4 sm:ml-64">
-      <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+      <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 overflow-y-scroll">
         <SelectedCity :selectedCity="selectedCity"/>
       </div>
     </div>
+
 
   </div>
 </template>
