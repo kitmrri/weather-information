0a35fda83cc274651d5a0df9311c5e325f2f5030
kitmrri <manuntagkit@gmail.com>
2023-02-08 16:22:57 +0900

    add background and icons provided by openweathermap

--- spa/src/components/SelectedCity.vue
+++ spa/src/components/SelectedCity.vue
@@ -1,35 +1,42 @@
 <template>
   <div class="h-screen flex flex-col widget">
-    <div class="text-lg text-center font-medium bg-gray-100">
+    <div class="text-lg text-center font-medium bg-sky-300">
       <h2>City: {{ selectedCity }}</h2>
       <h3>Current Weather:</h3>
       <div v-if="currentWeather.main">
         <p>{{ currentWeather.main.temp }} &#8451;</p>
-        <p>{{ currentWeather.weather[0].description }}</p>
+        <p class="text-transform: capitalize">
+          <img :src="'http://openweathermap.org/img/wn/' + currentWeather.weather[0].icon + '@2x.png'"
+              style="display: inline-block; vertical-align: middle;">
+          {{ currentWeather.weather[0].description }}
+        </p>
       </div>
     </div>
     <div class="flex h-full">
       <div class="flex-1 flex flex-col">
-        <div class="bg-slate-100 h-full flex-grow-0 overflow-y-auto overscroll-contain">
+        <div class="bg-sky-200 h-full flex-grow-0 overflow-y-auto overscroll-contain">
           <div class="bg-gray-300 text-center py-4">
             <h2 class="text-lg font-medium">5 Day Forecast:</h2>
           </div>   
           <div class="border rounded p-4 mb-4" v-for="(forecasts, date) in groupedForecasts" :key="date">
-            <h4>{{ date }}</h4>
             <ul>
               <li v-for="(forecast, index) in forecasts" :key="index">
-                {{ formatDate(forecast.dt) }} - {{ forecast.main.temp }} &#8451; - {{ forecast.weather[0].description }}
+                <p>{{ formatDate(forecast.dt) }} - {{ forecast.main.temp }} &#8451;</p>
+                <p class="text-transform: capitalize">
+                  <img :src="'http://openweathermap.org/img/wn/' + forecast.weather[0].icon + '@2x.png'" style="display: inline-block; vertical-align: middle;">
+                  {{ forecast.weather[0].description }}
+                </p>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="flex-1 bg-slate-200">
+      <div class="flex-1 bg-sky-200">
         <div class="bg-gray-300 text-center py-4">
           <h2 class="text-lg font-medium">Tourist Attractions</h2>
         </div>    
         <ul>
-          <li class="border rounded p-4 mb-4" v-for="information in placeInformation">
+          <li class="border rounded p-4 mb-4" v-for="(information, key) in placeInformation" :key="key">
             <p>Name: {{ information.name }}</p>
             <p>Address: {{ information.address_line }}</p>
           </li>
@@ -71,6 +78,9 @@ export default {
         grouped[date].push(forecast);
       });
       return grouped;
+    },
+    iconUrl() {
+      return `http://openweathermap.org/img/wn/${this.currentWeather.weather[0].icon}@2x.png`;
     }
   },
   methods: {
@@ -85,7 +95,7 @@ export default {
           this.currentWeather = currentWeatherResponse.data;
           this.fiveDayForecast = fiveDayForecastResponse.data;
           this.placeInformation = placeInformation.data
-          console.log(this.placeInformation)
+          console.log(this.fiveDayForecast)
         } catch (error) {
           console.error(error);
         }
@@ -98,3 +108,10 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.img{
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
--- spa/src/components/WeatherInformation.vue
+++ spa/src/components/WeatherInformation.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex">
-    <div class="w-1/4 p-4 border-r border-gray-300">
-      <h3 class="text-xl font-bold mb-4">Cities</h3>
+  <div class="flex bg-cover bg-center" style="background-image: url('./src/assets/background.jpg');">
+    <div class="w-1/4 p-4 border-r border-gray-300 bg-transparent text-white">
+      <h3 class="text-xl font-bold mb-4 ">Cities</h3>
       <ul class="list-group overflow-y-scroll max-h-screen min-h-screen">
-        <li class="list-group-item cursor-pointer hover:bg-gray-100 transition-all duration-200 ease-in-out p-4" v-for="city in cities" @click="selectCity(city)">
+        <li class="list-group-item cursor-pointer hover:bg-gray-400 transition-all duration-200 ease-in-out p-4" v-for="(city, key) in cities" @click="selectCity(city)" :key="key">
           {{ city }}
         </li>
       </ul>
     </div>
-    <div class="w-3/4 p-4">
+    <div class="w-3/4 p-4 bg-transparent">
       <SelectedCity :selectedCity="selectedCity"/>
     </div>
   </div>
