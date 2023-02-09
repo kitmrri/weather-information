cb75ad1c85a9e7e8fe4c39a63a69dbeb0099ca98
kitmrri <manuntagkit@gmail.com>
2023-02-09 14:31:20 +0900

    change color of tabs to black

--- spa/src/components/TabContainer.vue
+++ spa/src/components/TabContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+    <div class="text-sm font-medium text-center border-b border-gray-200 dark:text-black dark:border-gray-700">
       <ul class="flex flex-wrap -mb-px">
         <li class="mr-2">
           <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
