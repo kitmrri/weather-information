bb6e1879ee1aec3fd21c2fdfffc8c0f53c12b242
Keith MANUNTAG <keith@lograph.co.jp>
2023-02-09 19:35:33 +0900

    remove vite base

--- spa/vite.config.js
+++ spa/vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/weather-information/spa',
   plugins: [vue()],
   resolve: {
     alias: {
